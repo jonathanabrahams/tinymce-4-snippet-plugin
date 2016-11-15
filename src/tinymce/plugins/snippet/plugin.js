5b91f39081751c8333bf95a530a6ba0cfc9784dd
Vytautas Lozickas <v.lozickas@gmail.com>
2016-11-15 14:16:07 +0200

    Refactoring

--- src/tinymce/plugins/snippet/plugin.js
+++ src/tinymce/plugins/snippet/plugin.js
@@ -1,59 +1,53 @@
-tinymce.PluginManager.add('snippet', function (editor, url) {
-
-    editor.on('PreInit', function (e) {
-        var fileref = document.createElement("link");
-        fileref.setAttribute("rel", "stylesheet");
-        fileref.setAttribute("type", "text/css");
-        fileref.setAttribute("href", url + "/css/snippet.css");
-        document.getElementsByTagName("head")[0].appendChild(fileref);
-    });
-
+tinymce.PluginManager.add('snippet', function (editor) {
     var snippet_data = editor.getParam('snippet_list');
+
+    if (typeof snippet_data === "undefined") {
+        throw 'No snippet data provided';
+    }
+
+    editor.addButton('snippet', {
+        type: 'menubutton',
+        tooltip: 'Snippets',
+        icon: 'template',
+        menu: addItems(snippet_data)
+    });
 
     function addItems(data) {
         var menu = [];
-        for (var i = 0; i < data.length; i++) {
-            if (typeof data[i].title !== "undefined") {
-                var menuItem;
 
-                if (typeof data[i].items !== "undefined") {
-                    menuItem = {
-                        text: data[i].title,
-                        menu: addItems(data[i].items)
-                    };
-                }
-                else {
-                    menuItem = {
-                        text: data[i].title,
-                        onclick: function (item) {
-                            return function () {
-                                if(typeof item.value !== "undefined") {
-                                    editor.insertContent(item.value);
-                                }
-                                if (typeof item.onSelect === "function") {
-                                    item.onSelect(item);
-                                }
-                            }
-                        }(data[i])
-                    };
-                }
-                menu.push(menuItem);
+        for (var i = 0; i < data.length; i++) {
+            if (typeof data[i].title === "undefined") {
+                continue;
             }
+
+            var menuItem;
+
+            if (typeof data[i].items !== "undefined") {
+                menuItem = {
+                    text: data[i].title,
+                    menu: addItems(data[i].items)
+                };
+            } else {
+                menuItem = {
+                    text: data[i].title,
+                    onclick: onClickHandler(data[i])
+                };
+            }
+
+            menu.push(menuItem);
         }
 
         return menu;
     }
 
-    if (typeof snippet_data !== "undefined") {
-
-        editor.addButton('snippet', {
-            type: 'menubutton',
-            text: '',
-            tooltip: 'Snippets',
-            icon: false,
-            menu: addItems(snippet_data)
-        });
-
+    function onClickHandler(item) {
+        return function () {
+            if(typeof item.value !== "undefined") {
+                editor.insertContent(item.value);
+            }
+            if (typeof item.onSelect === "function") {
+                item.onSelect(item);
+            }
+        }
     }
-
 });
